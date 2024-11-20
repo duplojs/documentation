6bbd9a75c12d0b75fbcffdeed0b06dffc246b84b
mathcovax <98911237+mathcovax@users.noreply.github.com>
2024-11-20 15:36:38 +0100

    continue

--- _plugins/md_hl_syntax_to_liquid.rb
+++ _plugins/md_hl_syntax_to_liquid.rb
@@ -3,10 +3,8 @@
 # Convert markdown highlight lines feature syntax to liquid syntax
 
 Jekyll::Hooks.register :documents, :pre_render do |document, payload|
-    docExt = document.extname.tr('.', '')
-  
-    # only process if we deal with a markdown file
-    if payload['site']['markdown_ext'].include? docExt
-      document.content.gsub!(/^{([\d\s-]+)}\s\`\`\`([A-Za-z]+)$(.*?)^\`\`\`$/im, '{% highlight \2 highlight_lines="\1" %}\3{% endhighlight %}')
-    end
-  end
+  docExt = document.extname.tr('.', '')
+
+  # only process if we deal with a markdown file
+  document.content.gsub!(/^{([\d\s-]+)}\s\`\`\`([A-Za-z]+)$(.*?)^\`\`\`$/im, '{% highlight \2 highlight_lines="\1" %}\3{% endhighlight %}')
+end
