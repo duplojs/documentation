70da0ecbd35801d3894830543e6daef9b75f19ac
mathcovax <98911237+mathcovax@users.noreply.github.com>
2024-11-20 15:22:43 +0100

    continue

--- _plugins/md_hl_syntax_to_liquid.rb
+++ _plugins/md_hl_syntax_to_liquid.rb
@@ -7,6 +7,6 @@ Jekyll::Hooks.register :documents, :pre_render do |document, payload|
   
     # only process if we deal with a markdown file
     if payload['site']['markdown_ext'].include? docExt
-      document.content.gsub!(/^{([\d\s]+)}\`\`\`([A-Za-z]+)$(.*?)^\`\`\`$/im, '{% highlight \2 highlight_lines="\1" %}\3{% endhighlight %}')
+      document.content.gsub!(/^{([\d\s-]+)}\s\`\`\`([A-Za-z]+)$(.*?)^\`\`\`$/im, '{% highlight \2 highlight_lines="\1" %}\3{% endhighlight %}')
     end
   end
