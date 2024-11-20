dcf2fd4d104f476f1af07063fde4b3254266cf58
mathcovax <98911237+mathcovax@users.noreply.github.com>
2024-11-20 15:00:48 +0100

    continue

--- _plugins/md_hl_syntax_to_liquid.rb
+++ _plugins/md_hl_syntax_to_liquid.rb
@@ -2,31 +2,6 @@
 
 # Convert markdown highlight lines feature syntax to liquid syntax
 
-# Monkey-patch to allow highlighting lines
-module Jekyll
-  module Tags
-    class HighlightBlock
-      def render_rouge(code)
-        require "rouge"
-        formatter = Rouge::Formatters::HTMLLineHighlighter.new(
-          ::Rouge::Formatters::HTML.new,
-          highlight_lines: parse_highlighted_lines(@highlight_options[:highlight_lines])
-        )
-        lexer = ::Rouge::Lexer.find_fancy(@lang, code) || Rouge::Lexers::PlainText
-        formatter.format(lexer.lex(code))
-      end
-
-      private
-
-      def parse_highlighted_lines(lines_string)
-        return [] if lines_string.nil?
-
-        lines_string.map(&:to_i)
-      end
-    end
-  end
-end
-
 Jekyll::Hooks.register :documents, :pre_render do |document, payload|
     docExt = document.extname.tr('.', '')
   
